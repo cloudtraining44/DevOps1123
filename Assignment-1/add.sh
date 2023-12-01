77289e88f40f0531c010aed340255f95448f3a4a
Sohail Mohammed <cloudtraining44@gmail.com>
2023-11-30 21:17:06 -0600

    Modified add.sh

--- Assignment-1/add.sh
+++ Assignment-1/add.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 #Script to add two numbers
+
+
+#!/bin/bash
+echo "Enter first number"
+read x
+echo "Enter second number"
+read y
+echo "Enter the third number"
+read z
+
+(( sum=x+y+z ))
+echo "The result of addition=$sum"
+
