370567ea9209a5c73aefe674f328358f9a374d59
Sohail Mohammed <cloudtraining44@gmail.com>
2023-12-24 21:44:12 -0600

    Added terraform/excercise-2

---
+++ terraform/excercise-2/variables.tf
@@ -0,0 +1,30 @@
+variable "access_key" {
+  default = ""
+  type = string
+}
+
+variable "secret_key" {
+  default = ""
+  type = string
+}
+
+variable "ami" {
+  default = "ami-0759f51a90924c166"
+  type = string
+}
+
+variable "instance_type" {
+  
+}
+
+variable "tagname" {
+  
+}
+
+variable "Owner" {
+  
+}
+
+variable "sgname" {
+  
+}
