9965e207c0e5dc1a72b2b5673ec3f715462b4b8a
Khaja Naseeruddin Mohammed <mkn.naseeruddin@gmail.com>
2023-12-27 12:46:36 -0600

    Terraform-Exercise-1

---
+++ Terraform-Exercise-1/variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+			  default = "AKIA5AAYB2CX3QI3GZNV"
+			  type = string
+			}
+
+			variable "secret_key" {
+			  default = "12Y+4dab6OkltKQ/TBvT/14TUY0Vd8cHFmir0SIB"
+			  type = string
+			}
