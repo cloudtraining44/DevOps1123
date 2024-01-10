c439e16018964da0188a878fe3ef1059c763a488
Sohail Mohammed <cloudtraining44@gmail.com>
2024-01-10 14:06:09 -0600

    Modified variables.tf in testing-terraform

--- terraform/testing-terraform/variables.tf
+++ terraform/testing-terraform/variables.tf
@@ -1,12 +1,6 @@
-variable "access_key" {
-  default = "AKIA44EQJ67T5BA3LCPO"
-  type = string
-}
+variable "access_key" {}
 
-variable "secret_key" {
-  default = "3VzxBfRlFn5WNdZ5h8iZ+B7mZFCGFZUZfQRLlIYY"
-  type = string
-}
+variable "secret_key" {}
 
 variable "ami" {
   default = "ami-0759f51a90924c166"
