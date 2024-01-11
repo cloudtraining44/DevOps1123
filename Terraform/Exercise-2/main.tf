2977c99e94b34c406759caee2aadf1583b7357b4
Khaja Naseeruddin Mohammed <mkn.naseeruddin@gmail.com>
2024-01-11 07:58:34 -0600

    made changes

---
+++ Terraform/Exercise-2/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+
+resource "aws_instance" "demo-instance" {
+  ami                    = "ami-0759f51a90924c166"
+  instance_type          = "t2.micro"
+
+  //vpc_security_group_ids = [aws_security_group.web-sg-01.id]
+ # tags = {
+    #Name  = var.tagname
+    #Owner = "Terraform"
+  }
+
+#Security Group Resource to open port 80 
+resource "aws_security_group" "web-sg-01" {
+  name        = "Web-SG-01"
+  description = "Web-SG-01"
+
+    ingress {
+    description      = "ssh from Everywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
