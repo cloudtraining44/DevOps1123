2adaa6599b6bc36d30ca9e2ff88c88e917de12ab
Khaja Naseeruddin Mohammed <mkn.naseeruddin@gmail.com>
2024-01-16 14:07:10 -0600

    added sample_web jenkins

---
+++ Terraform/sample_web/main.tf
@@ -0,0 +1,41 @@
+#EC2 instance using UserData
+
+resource "aws_instance" "demo-instance" {
+  ami                    = "ami-0759f51a90924c166"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  user_data              = "${file("userdata_web.sh")}"
+  tags = {
+    Name  = "sohail-webserver"
+    Owner = "Terraform"
+  }
+}
+
+#Security Group Resource to open port 80 
+resource "aws_security_group" "web-sg" {
+  name        = "Web-SG"
+  description = "Web-SG"
+
+  dynamic ingress {
+    for_each = var.port
+
+    content {
+    description      = "from Everywhere"
+    from_port        = ingress.value
+    to_port          = ingress.value
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+ }
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.demo-instance.public_ip
+}
